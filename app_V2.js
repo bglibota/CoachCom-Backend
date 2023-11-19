c4c8e6826bad1bfe86397b2b754a2e0333935fb0
Ivan Giljević <igiljevic@student.foi.hr>
2023-11-19 15:24:23 +0100

    Implemented parts of backend - RESTful API - Client Registrationn - Trainer Registration - User (Client/Trainer) Login

---
+++ app_V2.js
@@ -0,0 +1,324 @@
+// const express = require('express');     //Loading express module
+import express from 'express';
+const app = express();                  //Objekt 'app' koji reprezentira "aplikaciju", sadrži različite korisne funk.
+
+import crypto from 'crypto';
+
+
+//Importing functions from the db.js file
+import {
+    checkIfUsernameIsAvailable,
+    getASpecificUser,
+    createANewClient,
+    createANewTrainer,
+    checkUsernameForLogin
+} from './db_V2.js';
+
+
+//Defining for the app to use JSON (since this is a JSON API)
+app.use(express.json())
+
+
+
+
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Registration -- ####################################################
+//--------------------------------------------------------------------------------------
+
+app.post("/API_V2/users/register/client", async (req, res) => {
+
+    try {
+
+        const expectedJSONObjectElements = [
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "e_mail",
+            "date_of_birth",
+            "phone_number",
+            "place_of_residence",
+            "sex",
+            "biography",
+            "profile_picture_path",
+            "biography_video_path"
+        ];
+
+        // Checks if all expected object elements are present in body of the request
+        const hasAllExpectedObjectElements = expectedJSONObjectElements.every(field => field in req.body);      //@IvanGiljević - Try to understand more clearely later...
+
+        if (!hasAllExpectedObjectElements) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid request body. Missing or unexpected object elements!',
+                data: []
+            });
+        }
+
+        const {
+            username,
+            password,
+            first_name,
+            last_name,
+            e_mail,
+            date_of_birth,
+            phone_number,
+            place_of_residence,
+            sex,
+            biography,
+            profile_picture_path,
+            biography_video_path
+        } = req.body;
+
+        //##### - TO DO - Server side data filtering (against SQL I and XSS attacks) (@MMatijević?)
+
+        if (!await checkIfUsernameIsAvailable(username)) {
+            res.status(409).json(
+                {
+                    success: false,
+                    message: "Chosen username already taken!",
+                    data: []
+                }
+            )
+        } else {
+
+            const userSalt = crypto.randomBytes(16).toString('hex') //Generates hex data that is store in 16 bytess
+            const safePassword = crypto.createHash('sha256').update(password + userSalt).digest('hex');
+
+            const newClient = await createANewClient(
+                username,
+                safePassword,
+                userSalt,
+                first_name,
+                last_name,
+                e_mail,
+                date_of_birth,
+                phone_number,
+                place_of_residence,
+                sex,
+                biography,
+                profile_picture_path,
+                biography_video_path,
+                password
+            )
+
+            res.status(201).json(
+                {
+                    success: true,
+                    message: "New client " + username + " created!",
+                    data: [newClient]
+                }
+            )
+        }
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: "Error - /API_V2/users/register/client - Error registering new client",
+                data: []
+            }
+        )
+    }
+});
+
+
+
+app.post("/API_V2/users/register/trainer", async (req, res) => {
+
+    try {
+
+        const expectedJSONObjectElements = [
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "e_mail",
+            "date_of_birth",
+            "phone_number",
+            "place_of_residence",
+            "sex",
+            "biography",
+            "profile_picture_path",
+            "documentation_directory_path",
+            "biography_video_path"
+        ];
+
+        // Checks if all expected object elements are present in body of the request
+        const hasAllExpectedObjectElements = expectedJSONObjectElements.every(field => field in req.body);      //@IvanGiljević - Try to understand more clearely later...
+
+        if (!hasAllExpectedObjectElements) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid request body. Missing or unexpected object elements!',
+                data: []
+            });
+        }
+
+        const {
+            username,
+            password,
+            first_name,
+            last_name,
+            e_mail,
+            date_of_birth,
+            phone_number,
+            place_of_residence,
+            sex,
+            biography,
+            profile_picture_path,
+            documentation_directory_path,
+            biography_video_path
+        } = req.body;
+
+        //##### - TO DO - Server side data filtering (against SQL I and XSS attacks) (@MMatijević?)
+
+        if (!await checkIfUsernameIsAvailable(username)) {
+            res.status(409).json(
+                {
+                    success: false,
+                    message: "Chosen username already taken!",
+                    data: []
+                }
+            )
+        } else {
+
+            const userSalt = crypto.randomBytes(16).toString('hex') //Generates hex data that is store in 16 bytess
+            const safePassword = crypto.createHash('sha256').update(password + userSalt).digest('hex');
+
+            const newTrainer = await createANewTrainer(
+                username,
+                safePassword,
+                userSalt,
+                first_name,
+                last_name,
+                e_mail,
+                date_of_birth,
+                phone_number,
+                place_of_residence,
+                sex,
+                biography,
+                profile_picture_path,
+                documentation_directory_path,
+                biography_video_path,
+                password
+            )
+
+            res.status(201).json(
+                {
+                    success: true,
+                    message: "New trainer " + username + " created!",
+                    data: [newTrainer]
+                }
+            )
+        }
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: "Error - /API_V2/users/register/trainer - Error registering new trainer",
+                data: []
+            }
+        )
+    }
+});
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Registration -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//--------------------------------------------------------------------------------------
+
+
+
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Login -- ###########################################################
+//--------------------------------------------------------------------------------------
+
+app.post("/API_V2/users/login", async (req, res) => {
+
+    try {
+
+        const expectedJSONObjectElements = [
+            "insertedUsername",
+            "insertedPassword"
+        ];
+
+        // Checks if all expected object elements are present in body of the request
+        const hasAllExpectedObjectElements = expectedJSONObjectElements.every(field => field in req.body);      //@IvanGiljević - Try to understand more clearely later...
+
+        if (!hasAllExpectedObjectElements) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid request body. Missing or unexpected object elements!',
+                data: []
+            });
+        }
+
+        const {
+            insertedUsername,
+            insertedPassword
+        } = req.body;
+
+        const searchedUser = await checkUsernameForLogin(insertedUsername)
+
+        if (searchedUser[0] == null) {       //queryResult[0]
+            res.status(404).json(
+                {
+                    success: false,
+                    message: "User with given username doesn't exist!",
+                    data: []
+                }
+            )
+        } else {
+
+            const hashUserSaltedInsertedPassword = crypto.createHash('sha256').update(insertedPassword + searchedUser[0].salt).digest('hex')
+
+            if (searchedUser[0].password != hashUserSaltedInsertedPassword) {
+                //######### TO DO - Increment number_of_login_attempts
+                res.status(401).json(
+                    {
+                        success: false,
+                        message: "User's password incorrect!",
+                        data: []
+                    }
+                )
+            } else {
+                //######### TO DO - set number_of_login_attempts to 0 AND set last_login_time
+                res.status(200).json(
+                    {
+                        success: true,
+                        message: "Successful login!",
+                        data: []                        //MYB send JWT token in future ?@MMatijević?
+                    }
+                )
+            }
+        }
+
+    } catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+                message: "Error - /API_V2/users/login - Error checking credentials",
+                data: []
+            }
+        )
+    }
+});
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Login -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//--------------------------------------------------------------------------------------
+
+
+//Error handling...
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Error - Something broke!')
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running and listening on port ${port} ...`)
+});
---
+++ db_V2.js
@@ -0,0 +1,176 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+import { generateRandomString } from './functions.js';
+
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Registration -- ####################################################
+//--------------------------------------------------------------------------------------
+
+export async function checkIfUsernameIsAvailable(insertedUsername) {
+    const [queryResult] = await pool.query(
+        `
+        SELECT *
+        FROM users
+        WHERE username = ?
+        LIMIT 1
+        `,
+        [insertedUsername]
+    );
+
+    if (queryResult[0] == null) {
+        return true     //username available
+    } else {
+        return false
+    }
+}
+// console.log(await checkIfUsernameIsAvailable("kkk"))
+
+export async function getASpecificUser(userId) {
+    const [queryResult] = await pool.query(
+        `
+        SELECT * 
+        FROM users
+        WHERE user_id = ?
+        LIMIT 1
+        `,
+        [userId]
+    );
+    return queryResult[0]   // [0] First element of an array
+}
+
+export async function createANewClient(
+    userName, safePassword, salt, firstName, lastName, eMail, dateOfBirth, phoneNumber, placeOfResidence, sex, biography,
+    profilePicturePath,
+    biographyVideoPath, rawPassword
+) {
+    const userTypeId = 2;
+    const activationCode = generateRandomString(8);
+    const profileStatus = true;
+    const numberOfLoginAttempts = 0;
+    const lastLoginTime = null;
+
+    const currentDateTime = new Date();
+
+    const year = currentDateTime.getFullYear();
+    const month = String(currentDateTime.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(currentDateTime.getDate()).padStart(2, '0');
+    const hours = String(currentDateTime.getHours()).padStart(2, '0');
+    const minutes = String(currentDateTime.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDateTime.getSeconds()).padStart(2, '0');
+
+    const currentDateTimeFormattedForSQL = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    const registrationDate = currentDateTimeFormattedForSQL;
+
+    const queryResult = await pool.query(
+        `
+        INSERT INTO users 
+        (
+            username, password, salt, first_name, last_name, e_mail, date_of_birth, phone_number, place_of_residence, sex, biography, 
+            registration_date, last_login_time, number_of_login_attempts, activation_code, profile_picture_path, 
+            profile_status, biography_video_path, user_type_id, raw_password
+        )
+        VALUES 
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `,
+        [
+            userName, safePassword, salt, firstName, lastName, eMail, dateOfBirth, phoneNumber, placeOfResidence, sex, biography,
+            registrationDate, lastLoginTime, numberOfLoginAttempts, activationCode, profilePicturePath,
+            profileStatus, biographyVideoPath, userTypeId, rawPassword
+        ]
+    );
+
+    const newClientId = queryResult[0].insertId;  //-- [0] must be defined here (to net get undefined) since you removed them at the start of the function...
+
+    return getASpecificUser(newClientId)
+}
+
+export async function createANewTrainer(
+    userName, safePassword, salt, firstName, lastName, eMail, dateOfBirth, phoneNumber, placeOfResidence, sex, biography,
+    profilePicturePath, documentationDirectoryPath,
+    biographyVideoPath, rawPassword
+) {
+    const userTypeId = 1;
+    const activationCode = generateRandomString(8);
+    const profileStatus = true;
+    const numberOfLoginAttempts = 0;
+    const lastLoginTime = null;
+
+    const currentDateTime = new Date();
+
+    const year = currentDateTime.getFullYear();
+    const month = String(currentDateTime.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(currentDateTime.getDate()).padStart(2, '0');
+    const hours = String(currentDateTime.getHours()).padStart(2, '0');
+    const minutes = String(currentDateTime.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDateTime.getSeconds()).padStart(2, '0');
+
+    const currentDateTimeFormattedForSQL = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    const registrationDate = currentDateTimeFormattedForSQL;
+
+    const queryResult = await pool.query(
+        `
+        INSERT INTO users 
+        (username, password, salt, first_name, last_name, e_mail, date_of_birth, phone_number, place_of_residence, sex, biography, 
+            registration_date, last_login_time, number_of_login_attempts, activation_code, profile_picture_path, documentation_directory_path, 
+            profile_status, biography_video_path, user_type_id, raw_password)
+        VALUES 
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `,
+        [
+            userName, safePassword, salt, firstName, lastName, eMail, dateOfBirth, phoneNumber, placeOfResidence, sex, biography,
+            registrationDate, lastLoginTime, numberOfLoginAttempts, activationCode, profilePicturePath, documentationDirectoryPath,
+            profileStatus, biographyVideoPath, userTypeId, rawPassword
+        ]
+    );
+
+    const newTrainerId = queryResult[0].insertId;  //-- [0] must be defined here (to net get undefined) since you removed them at the start of the function...
+
+    return getASpecificUser(newTrainerId)
+}
+
+//If needed - Formated version for pleasent croatian viewing...
+// console.log(new Date().toLocaleString('hr-HR', { timeZone: 'Europe/Zagreb', hour12: false }))
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Registration -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//--------------------------------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Login -- ###########################################################
+//--------------------------------------------------------------------------------------
+
+export async function checkUsernameForLogin(insertedUsername) {
+    const [queryResult] = await pool.query(
+        `
+        SELECT username, password, salt, raw_password
+        FROM users
+        WHERE username = ?
+        LIMIT 1
+        `,
+        [insertedUsername]
+    );
+
+    return queryResult
+}
+// console.log(await checkUsernameForLogin("nherci", "niki123"))
+
+//--------------------------------------------------------------------------------------
+//-- RESTful API -- Login -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//--------------------------------------------------------------------------------------
+
+
